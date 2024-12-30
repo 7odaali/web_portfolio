d6dbbc5c25e68a60d1131d2ca9c86834586724cb
RTX <7odaaliebaideng@gmail.com>
2024-12-30 16:55:35 +0200

    ..

--- lib/widgets/portfolio.dart
+++ lib/widgets/portfolio.dart
@@ -12,29 +12,54 @@ class _PortfolioState extends State<Portfolio> {
   Widget build(BuildContext context) {
     double w = MediaQuery.sizeOf(context).width;
     double h = MediaQuery.sizeOf(context).height;
+
+    int columns = (w > 1200)
+        ? 4
+        : (w > 700)
+            ? 3
+            : (w > 500)
+                ? 3
+                : (w > 400)
+                    ? 2
+                    : 1;
+
+    double itemHeight = (columns == 4)
+        ? 0.52 * h
+        : (columns == 3)
+            ? 0.42 * h
+            : (columns == 2)
+                ? 0.3 * h
+                : 0.3 * h;
+
+    double itemWidth = w / columns - 0.02 * w;
+
     return Column(
       children: [
         Text(
           "Portfolio",
           style: TextStyle(
               fontWeight: FontWeight.bold,
-              fontSize: 0.02 * w,
+              fontSize: 0.025 * w,
               color: Colors.black),
         ),
         Text(
           "Portfolio of some relevant projects:",
           style: TextStyle(
             fontWeight: FontWeight.normal,
-            fontSize: 0.01 * w,
+            fontSize: 0.017 * w,
             color: Colors.grey[400],
           ),
         ),
         SizedBox(height: 0.02 * h),
         Wrap(
-          children: [
-            Container(margin: EdgeInsets.all(0.01*w),
-              height: 0.4 * h,
-              width: 0.2 * w,
+          spacing: 0.01 * w,
+          runSpacing: 0.02 * h,
+          children: List.generate(
+            8,
+            (index) => Container(
+              width: itemWidth,
+              height: itemHeight,
+              margin: EdgeInsets.all(0.01 * w),
               decoration: BoxDecoration(
                 color: Colors.grey[300],
                 borderRadius: BorderRadius.circular(0.003 * w),
@@ -46,7 +71,7 @@ class _PortfolioState extends State<Portfolio> {
                     "Flutter App",
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 0.01 * w,
+                        fontSize: 0.017 * w,
                         color: Colors.green),
                   ),
                   SizedBox(height: 0.01 * h),
@@ -54,17 +79,17 @@ class _PortfolioState extends State<Portfolio> {
                     "Bazzar App",
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 0.016 * w,
+                        fontSize: 0.022 * w,
                         color: Colors.black),
                   ),
                   SizedBox(height: 0.01 * h),
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 0.01 * w),
                     child: Text(
-                      "By using firebase as a backend , I developed a flutter app for shoping online ,Cart ",
+                      "By using firebase as a backend, I developed a flutter app for shopping online, Cart",
                       style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 0.009 * w,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 0.016 * w,
                           color: Colors.black),
                     ),
                   ),
@@ -73,7 +98,7 @@ class _PortfolioState extends State<Portfolio> {
                     "Technologies Used",
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 0.01 * w,
+                        fontSize: 0.017 * w,
                         color: Colors.black),
                   ),
                   SizedBox(height: 0.01 * h),
@@ -85,8 +110,8 @@ class _PortfolioState extends State<Portfolio> {
                         child: Image.network(
                           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWiWY0E_du9TYa4Nd-XDhDJNjUrU6r6h31JQ&s",
                           width: 0.025 * w,
-                          height: 0.05 * h,
-                          fit: BoxFit.fill,
+                          height: 0.04 * h,
+                          fit: BoxFit.cover,
                         ),
                       ),
                       Padding(
@@ -94,8 +119,8 @@ class _PortfolioState extends State<Portfolio> {
                         child: Image.network(
                           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzzPL5MeLqdbE7bDka2QS8elhd8gn0L_h3Ug&s",
                           width: 0.025 * w,
-                          height: 0.05 * h,
-                          fit: BoxFit.fill,
+                          height: 0.04 * h,
+                          fit: BoxFit.cover,
                         ),
                       ),
                       Padding(
@@ -104,8 +129,8 @@ class _PortfolioState extends State<Portfolio> {
                           "https://www.gstatic.com/devrel-devsite/prod/v5"
                           "ab6fd0ad9c02b131b4d387b5751ac2c3616478c6dd65b5e931f0805efa1009c/firebase/images/touchicon-180.png",
                           width: 0.025 * w,
-                          height: 0.05 * h,
-                          fit: BoxFit.fill,
+                          height: 0.04 * h,
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ],
@@ -125,7 +150,7 @@ class _PortfolioState extends State<Portfolio> {
                         "GITHUB LINK",
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 0.01 * w,
+                            fontSize: 0.015 * w,
                             color: Colors.black),
                       ),
                     ),
@@ -134,111 +159,8 @@ class _PortfolioState extends State<Portfolio> {
                 ],
               ),
             ),
-            Container(margin: EdgeInsets.all(0.01*w),
-              height: 0.4 * h,
-              width: 0.2 * w,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(0.003 * w),
-              ),
-              child: Column(
-                children: [
-                  SizedBox(height: 0.01 * h),
-                  Text(
-                    "Flutter App",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 0.01 * w,
-                        color: Colors.green),
-                  ),
-                  SizedBox(height: 0.01 * h),
-                  Text(
-                    "Bazzar App",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 0.016 * w,
-                        color: Colors.black),
-                  ),
-                  SizedBox(height: 0.01 * h),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 0.01 * w),
-                    child: Text(
-                      "By using firebase as a backend , I developed a flutter app for shoping online ,Cart ",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 0.009 * w,
-                          color: Colors.black),
-                    ),
-                  ),
-                  SizedBox(height: 0.01 * h),
-                  Text(
-                    "Technologies Used",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 0.01 * w,
-                        color: Colors.black),
-                  ),
-                  SizedBox(height: 0.01 * h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: .01 * w),
-                        child: Image.network(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWiWY0E_du9TYa4Nd-XDhDJNjUrU6r6h31JQ&s",
-                          width: 0.025 * w,
-                          height: 0.05 * h,
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: .01 * w),
-                        child: Image.network(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzzPL5MeLqdbE7bDka2QS8elhd8gn0L_h3Ug&s",
-                          width: 0.025 * w,
-                          height: 0.05 * h,
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: .01 * w),
-                        child: Image.network(
-                          "https://www.gstatic.com/devrel-devsite/prod/v5"
-                              "ab6fd0ad9c02b131b4d387b5751ac2c3616478c6dd65b5e931f0805efa1009c/firebase/images/touchicon-180.png",
-                          width: 0.025 * w,
-                          height: 0.05 * h,
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: .04 * w),
-                    child: MaterialButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0.009 * w),
-                      ),
-                      minWidth: w,
-                      height: 0.07 * h,
-                      color: Colors.green,
-                      onPressed: () {},
-                      child: Text(
-                        "GITHUB LINK",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 0.01 * w,
-                            color: Colors.black),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 0.03 * h),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
-
       ],
     );
   }
